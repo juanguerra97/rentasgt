a63b57454e600464747523deae7ef7d0a3ba62b2
Juan Guerra <juanguerra97@protonmail.com>
2020-06-27 10:24:32 -0600

    added all entities db sets

--- src/Application/Common/Interfaces/IApplicationDbContext.cs
+++ src/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -11,6 +11,42 @@ namespace rentasgt.Application.Common.Interfaces
 
         DbSet<TodoItem> TodoItems { get; set; }
 
+        DbSet<AddressPicture> AddressPictures { get; set; }
+
+        DbSet<Category> Categories { get; set; }
+
+        DbSet<ChatMessage> ChatMessages { get; set; }
+
+        DbSet<ChatRoom> ChatRooms { get; set; }
+
+        DbSet<DpiPicture> DpiPictures { get; set; }
+
+        DbSet<Picture> Pictures { get; set; }
+
+        DbSet<Product> Products { get; set; }
+
+        DbSet<ProductCategory> ProductCategories { get; set; }
+
+        DbSet<ProductPicture> ProductPictures { get; set; }
+
+        DbSet<ProfilePicture> ProfilePictures { get; set; }
+
+        DbSet<Rent> Rents { get; set; }
+
+        DbSet<RentCost> RentCosts { get; set; }
+
+        DbSet<RentRequest> RentRequests { get; set; }
+
+        DbSet<RequestEvent> RequestEvents { get; set; }
+
+        DbSet<TextMessage> TextMessages { get; set; }
+
+        DbSet<UserChatRoom> UserChatRooms { get; set; }
+
+        DbSet<UserPicture> UserPictures { get; set; }
+
+        DbSet<UserProfileEvent> UserProfileEvents { get; set; }
+
         Task<int> SaveChangesAsync(CancellationToken cancellationToken);
     }
 }
