28df4237e31b0889ae7699d17380b4a3fb90be92
Juan Guerra <juanguerra97@protonmail.com>
2020-06-27 10:36:34 -0600

    Command and validator for creating a category

---
+++ src/Application/Categories/Commands/CreateCategory/CreateCategoryCommand.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using MediatR;
+using rentasgt.Application.Common.Interfaces;
+using rentasgt.Domain.Entities;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace rentasgt.Application.Categories.Commands.CreateCategory
+{
+    public partial class CreateCategoryCommand : IRequest<long>
+    {
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+
+    public class CreateCategoryCommandHandler : IRequestHandler<CreateCategoryCommand, long>
+    {
+
+        private readonly IApplicationDbContext context;
+        private readonly Mapper mapper;
+
+        public CreateCategoryCommandHandler(IApplicationDbContext context, Mapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public async Task<long> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
+        {
+
+            var newCategory = mapper.Map<CreateCategoryCommand, Category>(request);
+
+            await this.context.Categories.AddAsync(newCategory);
+            await this.context.SaveChangesAsync(cancellationToken);
+
+            return newCategory.Id;
+
+        }
+    }
+
+}
---
+++ src/Application/Categories/Commands/CreateCategory/CreateCategoryCommandValidator.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using rentasgt.Application.Common.Interfaces;
+using rentasgt.Domain.Entities;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace rentasgt.Application.Categories.Commands.CreateCategory
+{
+    public class CreateCategoryCommandValidator : AbstractValidator<CreateCategoryCommand>
+    {
+
+        private readonly IApplicationDbContext _context;
+
+        public CreateCategoryCommandValidator(IApplicationDbContext context)
+        {
+            _context = context;
+
+            RuleFor(c => c.Name)
+                .NotEmpty().WithMessage("El nombre es obligatorio")
+                .MaximumLength(Category.MAX_NAME_LENGTH).WithMessage($"El nombre no debe sobrepasar los {Category.MAX_NAME_LENGTH} caracteres")
+                .MustAsync(BeUniqueName).WithMessage("El nombre especificado ya existe");
+
+            RuleFor(c => c.Description)
+                .NotEmpty().WithMessage("La descripcion es obligatoria")
+                .MaximumLength(Category.MAX_DESCRIPTION_LENGTH).WithMessage($"La descripcion no debe sobrepasar los {Category.MAX_DESCRIPTION_LENGTH} caracteres");
+
+        }
+
+        public async Task<bool> BeUniqueName(string name, CancellationToken cancellationToken)
+        {
+            return await _context.Categories
+                .AllAsync(c => c.Name != name);
+        }
+
+    }
+}
