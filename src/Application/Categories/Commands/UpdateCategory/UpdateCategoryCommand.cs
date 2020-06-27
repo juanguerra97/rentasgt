d8cfa6e5e17a0c5abe35c9dcc3511292b4b08e95
Juan Guerra <juanguerra97@protonmail.com>
2020-06-27 10:37:17 -0600

    Command and validator for updating a category

---
+++ src/Application/Categories/Commands/UpdateCategory/UpdateCategoryCommand.cs
@@ -0,0 +1,58 @@
+﻿using MediatR;
+using rentasgt.Application.Common.Exceptions;
+using rentasgt.Application.Common.Interfaces;
+using rentasgt.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace rentasgt.Application.Categories.Commands.UpdateCategory
+{
+    public partial class UpdateCategoryCommand : IRequest
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+
+    public class UpdateCategoryCommandHandler : IRequestHandler<UpdateCategoryCommand>
+    {
+
+        private readonly IApplicationDbContext context;
+
+        public UpdateCategoryCommandHandler(IApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Unit> Handle(UpdateCategoryCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await this.context.Categories.FindAsync(request.Id);
+
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(Category), request.Id);
+            }
+            
+            if (request.Name != null)
+            {
+                entity.Name = request.Name;
+            }
+
+            if (request.Description != null)
+            {
+                entity.Description = request.Description;
+            }
+
+            if (request.Name != null || request.Description != null)
+            {
+                await this.context.SaveChangesAsync(cancellationToken);
+            }
+
+            return Unit.Value;
+        }
+    }
+
+}
---
+++ src/Application/Categories/Commands/UpdateCategory/UpdateCategoryCommandValidator.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using rentasgt.Application.Common.Interfaces;
+using rentasgt.Domain.Entities;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace rentasgt.Application.Categories.Commands.UpdateCategory
+{
+    public class UpdateCategoryCommandValidator : AbstractValidator<UpdateCategoryCommand>
+    {
+
+        private readonly IApplicationDbContext context;
+
+        public UpdateCategoryCommandValidator(IApplicationDbContext context)
+        {
+            this.context = context;
+
+            RuleFor(c => c.Name)
+                .NotEmpty().WithMessage("El nombre es obligatorio")
+                .MaximumLength(Category.MAX_NAME_LENGTH)
+                    .WithMessage($"El nombre no debe sobrepasar los {Category.MAX_NAME_LENGTH} caracteres")
+                .MustAsync(BeUniqueName).WithMessage("El nombre especificado ya existe")
+                .When(c => c.Name != null);
+
+            RuleFor(c => c.Description)
+                .NotEmpty().WithMessage("La descripcion es obligatoria")
+                .MaximumLength(Category.MAX_DESCRIPTION_LENGTH)
+                    .WithMessage($"La descripcion no debe sobrepasar los {Category.MAX_DESCRIPTION_LENGTH} caracteres")
+                .When(c => c.Description != null);
+        }
+
+        public async Task<bool> BeUniqueName(string name, CancellationToken cancellationToken)
+        {
+            return await this.context.Categories
+                .AllAsync(c => c.Name != name);
+        }
+
+    }
+}
