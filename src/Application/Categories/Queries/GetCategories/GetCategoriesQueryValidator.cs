78b816b7d9ece096955800675d8fa456a6a6cc06
Juan Guerra <juanguerra97@protonmail.com>
2020-06-28 10:59:28 -0600

    Validator for GetCategoriesQuery

---
+++ src/Application/Categories/Queries/GetCategories/GetCategoriesQueryValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace rentasgt.Application.Categories.Queries.GetCategories
+{
+    public class GetCategoriesQueryValidator : AbstractValidator<GetCategoriesQuery>
+    {
+        public GetCategoriesQueryValidator()
+        {
+            RuleFor(r => r.PageNumber)
+                .GreaterThan(0).WithMessage("El numero de pagina debe ser un entero positivo");
+            RuleFor(r => r.PageSize)
+                .InclusiveBetween(5, 100).WithMessage("El tamaño de la pagina debe ser entre 5 y 100");
+        }
+    }
+}
