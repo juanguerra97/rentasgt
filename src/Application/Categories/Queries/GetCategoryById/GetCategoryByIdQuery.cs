a7915079d0ae764f615ca5b369121a40c8551d39
Juan Guerra <juanguerra97@protonmail.com>
2020-06-28 10:54:54 -0600

    Query and validator for retrieving a single category by its id

---
+++ src/Application/Categories/Queries/GetCategoryById/GetCategoryByIdQuery.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using rentasgt.Application.Categories.Queries.GetCategories;
+using rentasgt.Application.Common.Exceptions;
+using rentasgt.Application.Common.Interfaces;
+using rentasgt.Domain.Entities;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace rentasgt.Application.Categories.Queries.GetCategoryById
+{
+    public class GetCategoryByIdQuery : IRequest<CategoryDto>
+    {
+
+        public long Id { get; set; }
+
+    }
+
+    public class GetCategoryByIdQueryHandler : IRequestHandler<GetCategoryByIdQuery, CategoryDto>
+    {
+
+        private readonly IApplicationDbContext context;
+        private readonly Mapper mapper;
+
+        public GetCategoryByIdQueryHandler(IApplicationDbContext context, Mapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public async Task<CategoryDto> Handle(GetCategoryByIdQuery request, CancellationToken cancellationToken)
+        {
+
+            Category entity = await this.context.Categories.FirstOrDefaultAsync(c => c.Id == request.Id);
+
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(Category), request.Id);
+            }
+
+            return this.mapper.Map<Category, CategoryDto>(entity);
+
+        }
+    }
+
+}
---
+++ src/Application/Categories/Queries/GetCategoryById/GetCategoryByIdValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+namespace rentasgt.Application.Categories.Queries.GetCategoryById
+{
+    public class GetCategoryByIdValidator : AbstractValidator<GetCategoryByIdQuery>
+    {
+        public GetCategoryByIdValidator()
+        {
+            RuleFor(r => r.Id)
+                .GreaterThan(0).WithMessage("El Id debe ser un entero positivo");
+        }
+    }
+}
