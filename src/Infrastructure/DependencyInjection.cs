e886f21f271c88f860972321d28c3bbd063b35db
Juan Guerra <juanguerra97@protonmail.com>
2020-07-02 14:12:56 -0600

    Created OnlyAdmin profile and implemented IProfileServer to include user roles in jwt token

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -9,6 +9,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using rentasgt.Domain.Entities;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using IdentityServer4.Services;
+using IdentityModel;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
 
 namespace rentasgt.Infrastructure
 {
@@ -33,17 +38,51 @@ namespace rentasgt.Infrastructure
 
             services.AddDefaultIdentity<AppUser>()
                 .AddRoles<IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>();                    
-            
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
             services.AddIdentityServer()
-                .AddApiAuthorization<AppUser, ApplicationDbContext>();
+                .AddApiAuthorization<AppUser, ApplicationDbContext>(configure =>
+                {
+                    //configure.IdentityResources.Add(new IdentityServer4.Models.IdentityResource(
+                    //    "roles", "Roles", new string[] { JwtClaimTypes.Role }));
+                    //foreach (var c in configure.Clients)
+                    //{
+                    //    if (c.ClientId == "rentasgt.WebUI")
+                    //    {
+                    //        c.AllowedScopes.Add("roles");
+                    //    }
+                    //}
+                });
+
+            services.AddTransient<IProfileService, IdentityClaimsProfileService>();
 
             services.AddTransient<IDateTime, DateTimeService>();
             services.AddTransient<IIdentityService, IdentityService>();
-            services.AddTransient<ICsvFileBuilder, CsvFileBuilder>();
+            services.AddTransient<ICsvFileBuilder, CsvFileBuilder>();            
 
-            services.AddAuthentication()
-                .AddIdentityServerJwt();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddIdentityServerJwt()
+                .AddJwtBearer(x =>
+                {
+                    x.RequireHttpsMetadata = false;
+                    x.SaveToken = true;
+                    x.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = false,
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
+
+            services.AddAuthorization(config =>
+            {
+                config.AddPolicy("OnlyAdmin",
+                    options => {
+                        options.RequireAssertion(context => {
+                            return context.User.HasClaim(c => (c.Type == ClaimTypes.Role || c.Type == JwtClaimTypes.Role) && c.Value == "Admin");
+                        });
+                        });
+            });
 
             return services;
         }
---
+++ src/Infrastructure/Identity/IdentityClaimsProfileService.cs
@@ -0,0 +1,52 @@
+﻿using IdentityModel;
+using IdentityServer4.Extensions;
+using IdentityServer4.Models;
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Identity;
+using rentasgt.Domain.Entities;
+using rentasgt.Domain.Enums;
+using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace rentasgt.Infrastructure.Identity
+{
+    public class IdentityClaimsProfileService : IProfileService
+    {
+        private readonly IUserClaimsPrincipalFactory<AppUser> _claimsFactory;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public IdentityClaimsProfileService(UserManager<AppUser> userManager, 
+            RoleManager<IdentityRole> roleManager,
+            IUserClaimsPrincipalFactory<AppUser> claimsFactory)
+        {
+            _userManager = userManager;
+            _claimsFactory = claimsFactory;
+            _roleManager = roleManager;
+        }
+
+        public async Task GetProfileDataAsync(ProfileDataRequestContext context)
+        {
+            var sub = context.Subject.GetSubjectId();
+            var user = await _userManager.FindByIdAsync(sub);
+            var principal = await _claimsFactory.CreateAsync(user);
+            var roles = await _userManager.GetRolesAsync(user);
+
+            var claims = principal.Claims.ToList();
+            claims = claims.Where(claim => context.RequestedClaimTypes.Contains(claim.Type)).ToList();
+            // note: to dynamically add roles (ie. for users other than consumers - simply look them up by sub id
+            claims.AddRange(roles.Select(r => new Claim(JwtClaimTypes.Role, r))); // need this for role-based authorization - https://stackoverflow.com/questions/40844310/role-based-authorization-with-identityserver4
+
+            context.IssuedClaims = claims;
+        }
+
+        public async Task IsActiveAsync(IsActiveContext context)
+        {
+            var sub = context.Subject.GetSubjectId();
+            var user = await _userManager.FindByIdAsync(sub);
+            context.IsActive = user != null && user.ProfileStatus != UserProfileStatus.Inactive;
+        }
+    }
+}
