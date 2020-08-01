185d2720df136806c5ec309950656382f2de5abf
Juan Guerra <juanguerra97@protonmail.com>
2020-08-01 17:39:19 -0600

    Command to allow the cancellation of a rent by the requestor

---
+++ src/Application/Rents/Commands/CancelRent/CancelRentCommand.cs
@@ -0,0 +1,61 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using rentasgt.Application.Common.Exceptions;
+using rentasgt.Application.Common.Interfaces;
+using rentasgt.Domain.Entities;
+using rentasgt.Domain.Enums;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace rentasgt.Application.Rents.Commands.CancelRent
+{
+    public class CancelRentCommand : IRequest
+    {
+        public long RentId { get; set; }
+    }
+
+    public class CancelRentCommandHandler : IRequestHandler<CancelRentCommand>
+    {
+
+        private readonly IApplicationDbContext context;
+        private readonly ICurrentUserService currentUserService;
+
+        public CancelRentCommandHandler(IApplicationDbContext context, ICurrentUserService currentUserService)
+        {
+            this.context = context;
+            this.currentUserService = currentUserService;
+        }
+
+        public async Task<Unit> Handle(CancelRentCommand request, CancellationToken cancellationToken)
+        {
+
+            var entity = await this.context.Rents
+                .Include(r => r.Request)
+                .ThenInclude(rq => rq.Product)
+                .ThenInclude(p => p.Owner)
+                .SingleOrDefaultAsync(r => r.RequestId == request.RentId);
+
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(Rent), request.RentId);
+            }
+
+            if (entity.Status != RentStatus.Pending)
+            {
+                throw new InvalidStateException($"No se puede iniciar una renta en estado {entity.Status}");
+            }
+
+            var userId = this.currentUserService.UserId;
+            if (userId != entity.Request.RequestorId)
+            {
+                throw new OperationForbidenException("No puedes cancelar esta renta");
+            }
+
+            entity.Status = RentStatus.Cancelled;
+            await this.context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+
+}
