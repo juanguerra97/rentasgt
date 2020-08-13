824d5920fa83ce2f7ee7cedb29982c6af00e25f2
Juan Guerra <juanguerra97@protonmail.com>
2020-08-13 12:05:58 -0600

    Created ImgCropperComponent and imported FileUploadModule

--- src/WebUI/ClientApp/src/app/app-common/app-common.module.ts
+++ src/WebUI/ClientApp/src/app/app-common/app-common.module.ts
@@ -1,26 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginatorNavComponent } from './paginator-nav/paginator-nav.component';
-import { ButtonModule } from 'primeng';
+import { ButtonModule, FileUploadModule } from 'primeng';
 import { ConfirmationModalComponent } from './confirmation-modal/confirmation-modal.component';
 import { ModalModule } from 'ngx-bootstrap/modal';
+import { ImgCropperComponent } from './img-cropper/img-cropper.component';
 
 @NgModule({
   declarations: [
     PaginatorNavComponent,
-    ConfirmationModalComponent
+    ConfirmationModalComponent,
+    ImgCropperComponent
   ],
   imports: [
     CommonModule,
     ButtonModule,
+    FileUploadModule,
     ModalModule.forRoot()
   ],
   exports: [
     PaginatorNavComponent,
     ConfirmationModalComponent,
+    ImgCropperComponent,
     CommonModule,
     ModalModule,
-    ButtonModule
+    ButtonModule,
+    FileUploadModule,
   ]
 })
 export class AppCommonModule { }
