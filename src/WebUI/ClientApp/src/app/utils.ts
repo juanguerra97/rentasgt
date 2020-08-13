b7b19939f906f40544edfe85873e3838564e0645
Juan Guerra <juanguerra97@protonmail.com>
2020-08-13 13:11:48 -0600

    created function imgBlobToBase64()

--- src/WebUI/ClientApp/src/app/utils.ts
+++ src/WebUI/ClientApp/src/app/utils.ts
@@ -10,3 +10,14 @@ export function getErrorsFromResponse(errorResponse): string[] {
   }
   return errors;
 }
+
+export function imgBlobToBase64(imgBlob: Blob): Promise<string|ArrayBuffer> {
+  return new Promise<string|ArrayBuffer>((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      resolve(reader.result);
+    };
+    reader.onerror = reject;
+    reader.readAsDataURL(imgBlob);
+  });
+}
