7949593961c9cbfc265975b4b23cc0b604dde237
Juan Guerra <juanguerra97@protonmail.com>
2020-08-24 22:34:05 -0600

    endpoint to get messages of a chatroom

--- src/WebUI/Controllers/ChatRoomsController.cs
+++ src/WebUI/Controllers/ChatRoomsController.cs
@@ -1,7 +1,9 @@
 using System.Threading.Tasks;
+using Application.ChatRooms.Queries.GetMessagesOfRoom;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using rentasgt.Application.ChatRooms.Queries.GetChatRoomsOfUser;
+using rentasgt.Application.ChatRooms.Queries.GetMessagesOfRoom;
 using rentasgt.Application.Common.Models;
 using rentasgt.WebUI.Controllers;
 
@@ -12,11 +14,24 @@ namespace WebUI.Controllers
     public class ChatRoomsController : ApiController
     {
         
+        [HttpGet]
         public async Task<ActionResult<PaginatedListResponse<ChatRoomDto>>> Get(
-            int pageSize = 15, int pageNumber = 1) {
+            int pageSize = 25, int pageNumber = 1) {
             return await Mediator.Send(new GetChatRoomsOfUserQuery {
                 PageSize = pageSize,
                 PageNumber = pageNumber,
+            });
+        }
+
+        [HttpGet("{id}/messages")]
+        public async Task<ActionResult<PaginatedListResponse<ChatMessageDto>>> GetMessages(
+            long id,
+            int pageSize = 35, int pageNumber = 1
+        ) {
+            return await Mediator.Send(new GetMessagesOfRoomQuery {
+                PageSize = pageSize,
+                PageNumber = pageNumber,
+                ChatRoomId = id
             });
         }
 
